b4694618573550b4a3f5802dc67eaccb7b0cc757
Gintaras <sytakeris@gmail.com>
2019-07-23 22:41:58 +0300

    add source version

---
+++ src/main/java/com/pricing/Main.java
@@ -0,0 +1,27 @@
+package com.pricing;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        var basket = new ShoppingBasket();
+
+        basket.addItem(new UnitItem("Beans", new BigDecimal(0.5)));
+        basket.addItem(new UnitItem("Beans", new BigDecimal(0.5)));
+        basket.addItem(new UnitItem("Beans", new BigDecimal(0.5)));
+
+        basket.addItem(new UnitItem("Coke", new BigDecimal(0.7)));
+        basket.addItem(new UnitItem("Coke", new BigDecimal(0.7)));
+
+        basket.addItem(new WeightedItem("Oranges", new BigDecimal(0.200), new BigDecimal(1.99)));
+
+        Discount beanDiscount = new PairDiscount("Beans 3 for 2", "Beans", 3, price -> price.multiply(new BigDecimal(3)).subtract(price.multiply(new BigDecimal(2))));
+        Discount cokeDiscount = new PairDiscount("Coke 2 for £1", "Coke", 2, price -> price.multiply(new BigDecimal(2)).subtract(BigDecimal.ONE));
+
+        var receipt = basket.checkout(List.of(beanDiscount, cokeDiscount));
+
+        System.out.println("Test");
+
+    }
+}
